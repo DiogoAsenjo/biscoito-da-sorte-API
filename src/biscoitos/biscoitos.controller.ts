0911d70000fb7c011863a687520fb81688d137f9
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-14 14:41:49 -0300

    Criando a página inicial

--- src/biscoitos/biscoitos.controller.ts
+++ src/biscoitos/biscoitos.controller.ts
@@ -1,10 +1,27 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common';
+import { Delete } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res,  } from '@nestjs/common';
 import { BiscoitosService } from './biscoitos.service';
 import { Response } from 'express'
 
 @Controller('biscoitos')
 export class BiscoitosController {
     constructor(private readonly biscoito: BiscoitosService) {}
+
+    @Get()
+    paginaInicial(): string {
+        return `<body>
+        <h1>Bem-vindo à Aplicação dos Biscoitos da Sorte!</h1>
+        <p>URLs disponíveis:</p>
+        <ul>
+            <li>positivos</li>
+            <li>listar-positivos</li>
+            <li>adicionar-positivo</li>
+            <li>negativos</li>
+            <li>listar-negativos</li>
+            <li>adicionar-negativo</li>
+        </ul>
+    </body>`
+    }
 
     @Get('positivos')
     imprimeMensagemPositiva(): string {
@@ -15,6 +32,11 @@ export class BiscoitosController {
     imprimeMensagemNegativa(): string {
         return this.biscoito.pegarBiscoitoDoMal();
     }
+
+    /* @Get('listar-positivas')
+    imprimePositivas(): Array<string> {
+        return this.biscoito
+    } */
 
     @Post('adicionar-positivo')
     adicionarMensagemPositiva(@Res() res: Response, @Body() body: { mensagem: string }): void {
@@ -27,6 +49,6 @@ export class BiscoitosController {
     adicionarMensagemNegativa(@Res() res: Response, @Body() body: { mensagem: string }): void {
         const fraseNegativa = body.mensagem;
         const novoArrayNegativo = this.biscoito.adicionarMensagemDoMal(fraseNegativa);
-        res.send(novoArrayNegativo);
+        res.send(`${body.mensagem} adicionada com sucesso!`);
     }
 }
--- src/biscoitos/biscoitos.service.ts
+++ src/biscoitos/biscoitos.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 const frasesPositivas: Array<string> = [
     "A sorte acompanhará todos os seus passos.",
-    "Grandes realizações exigem pequenos passos.",
+    /* "Grandes realizações exigem pequenos passos.",
     "O sucesso está a caminho, continue persistindo.",
     "Seu sorriso iluminará o dia de alguém.",
     "Acredite em si mesmo e tudo será possível.",
@@ -10,12 +10,12 @@ const frasesPositivas: Array<string> = [
     "A jornada é tão importante quanto o destino.",
     "A generosidade traz grande recompensa.",
     "Sua criatividade resolverá muitos problemas.",
-    "O aprendizado é a chave para o crescimento contínuo."
+    "O aprendizado é a chave para o crescimento contínuo." */
   ];
 
 const frasesNegativas: Array<string> = [
     "O sucesso é para os outros, não para você.",
-    "Não importa o quanto você tente, nunca será bom o suficiente.",
+    /* "Não importa o quanto você tente, nunca será bom o suficiente.",
     "A vida é uma sequência de decepções.",
     "Nada que você faz realmente importa no final das contas.",
     "Você está destinado a falhar, então por que se esforçar?",
@@ -23,7 +23,7 @@ const frasesNegativas: Array<string> = [
     "Cada novo dia traz mais tristeza do que o anterior.",
     "Seus sonhos são inalcançáveis, é melhor desistir agora.",
     "A esperança é uma ilusão que só leva à desilusão.",
-    "A vida é uma série interminável de monotonias e aborrecimentos."
+    "A vida é uma série interminável de monotonias e aborrecimentos." */
   ];
 
 @Injectable()
