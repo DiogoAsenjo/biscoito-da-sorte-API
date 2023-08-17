318734ba6abe9eecd65c063e60b856f0b29a7995
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-17 10:17:55 -0300

    Criando o módulo do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BiscoitosModule } from './biscoitos/biscoitos.module';
+import { BancoDeDadosModule } from './banco-de-dados/banco-de-dados.module';
 
 @Module({
-  imports: [BiscoitosModule],
+  imports: [BiscoitosModule, BancoDeDadosModule],
   controllers: [],
   providers: [],
 })
