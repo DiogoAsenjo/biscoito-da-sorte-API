a7d3be5ed0d80d1023442a8541817bf6b2396838
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-17 14:28:17 -0300

    Fazer a primeira rota POST para adicionar dados ao banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { BiscoitosModule } from './biscoitos/biscoitos.module';
 import { BancoDeDadosModule } from './banco-de-dados/banco-de-dados.module';
 import { CookiesModule } from './cookies/cookies.module';
+import { BiscoitosController } from './biscoitos/biscoitos.controller';
+import { CookiesController } from './cookies/cookies.controller';
+import { BiscoitosNegativosService } from './biscoitos/biscoitos.negativos.service';
+import { BiscoitosPositivosService } from './biscoitos/biscoitos.positivos.service';
 
 @Module({
   imports: [BiscoitosModule, BancoDeDadosModule],
-  controllers: [],
-  providers: [],
+  controllers: [BiscoitosController, CookiesController],
+  providers: [BiscoitosNegativosService, BiscoitosPositivosService],
 })
 export class AppModule {}
--- src/cookies/cookies.controller.ts
+++ src/cookies/cookies.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Post, Res } from '@nestjs/common';
+import { Cookies } from './cookie.entity';
+import { Response } from 'express';
 
 @Controller('cookies')
-export class CookiesController {}
+export class CookiesController {
+
+    @Get()
+    paginaInicial(): string {
+        return 'Bem-vindo a página inicial!'
+    }
+    
+    @Post('novo-cookie')
+    async adicionarCookiePositivo(@Res() res: Response, @Body() body: { mensagem: string }): Promise<void> {
+        const frasePositiva= body.mensagem;
+        await Cookies.create({frase: frasePositiva});
+        res.status(HttpStatus.CREATED).send('Frase adicionada com sucesso!')
+    }
+
+}
