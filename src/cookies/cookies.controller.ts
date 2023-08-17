021465fe9014fcd2278c402b7ad69601b0b3e933
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-17 15:04:04 -0300

    Criando a rota para mostrar todos os cookies do banco de dados

--- src/cookies/cookies.controller.ts
+++ src/cookies/cookies.controller.ts
@@ -10,11 +10,27 @@ export class CookiesController {
         return 'Bem-vindo a página inicial!'
     }
     
+
+    //CREATE
     @Post('novo-cookie')
     async adicionarCookiePositivo(@Res() res: Response, @Body() body: { mensagem: string }): Promise<void> {
         const frasePositiva= body.mensagem;
         await Cookies.create({frase: frasePositiva});
         res.status(HttpStatus.CREATED).send('Frase adicionada com sucesso!')
     }
+
+    //READ
+    @Get('mostrar-cookie')
+    async mostraMensagem(@Res() res: Response): Promise<void> {
+        try {
+            const cookies = await Cookies.findAll();
+            const frasesCookies = cookies.map((item) => item.frase);
+            res.status(HttpStatus.OK).send(frasesCookies);
+        } catch(erro) {
+            console.log(erro);
+            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(erro);
+        }
+    }
+
 
 }
