e1e47f89c4934b5d025d5c4adfb3e30b5f85a250
DiogoAsenjo <diogoasenjo@outlook.com>
2023-08-17 10:25:56 -0300

    Configurando o banco de dados

---
+++ src/banco-de-dados/banco.config.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+  {
+    provide: 'SEQUELIZE',
+    useFactory: async () => {
+      const sequelize = new Sequelize({
+        dialect: 'postgres',
+        host: 'localhost',
+        port: 5432, // A porta padrão do PostgreSQL
+        username: 'postgres',
+        password: '123456',
+        database: 'postgres',
+      });
+      try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+    },
+  },
+];
